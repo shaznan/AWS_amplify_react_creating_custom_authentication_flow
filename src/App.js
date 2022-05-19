34ee73ea1931aeb76b2c30d67630dd4c2cfd0826
Shaznan <shaznanfairozesally@gmail.com>
2022-05-19 22:54:02 +0530

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,102 +1,96 @@
-import './App.css';
-import React, {useState} from 'react'
-import { Auth } from 'aws-amplify'
-import NavigationBar from './components/navigation/NavigationBar';
-import Login from './container/Login/Login';
+import "./App.css";
+import React, { useState } from "react";
+import { Auth } from "aws-amplify";
+import NavigationBar from "./components/navigation/NavigationBar";
+import Login from "./container/Login/Login";
 
 const initialFormState = {
-   password: "", email:"", authCode: "", formType: 'signup'
-}
-
+  password: "",
+  email: "",
+  authCode: "",
+  formType: "signup",
+};
 
 function App() {
-const [formState, updateFormState] = useState(initialFormState)
+  const [formState, updateFormState] = useState(initialFormState);
 
-  const {formType} = formState
+  const { password, email, authCode, formType } = formState;
 
-  const signUp = async ()=>{
+  const signUp = async () => {
     try {
-      const {password, email} = formState
-      const {user} = await Auth.signUp({
+      const { user } = await Auth.signUp({
         username: email,
         password,
-      })
-      console.log(user)
+      });
+      console.log(user);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-    updateFormState(()=>({...formState, formType: "confirmSignUp"}))
-  }
+    updateFormState(() => ({ ...formState, formType: "confirmSignUp" }));
+  };
 
   async function confirmSignUp() {
     try {
-      const { authCode, email} = formState
       await Auth.confirmSignUp(email, authCode);
     } catch (error) {
-        console.log('error confirming sign up', error);
+      console.log("error confirming sign up", error);
     }
-    updateFormState(()=>({...formState, formType: "signin"}))
-
-}
-
-async function signIn() {
-  const { password, email} = formState
-
-  try {
-      const user = await Auth.signIn(email, password);
-      console.log(user)
-  } catch (error) {
-      console.log('error signing in', error);
+    updateFormState(() => ({ ...formState, formType: "signin" }));
   }
-  updateFormState(()=>({...formState, formType: "welcomeScreen"}))
 
-}
+  async function signIn() {
+    try {
+      const user = await Auth.signIn(email, password);
+      console.log(user);
+    } catch (error) {
+      console.log("error signing in", error);
+    }
+    updateFormState(() => ({ ...formState, formType: "welcomeScreen" }));
+  }
 
-const onChange = (e)=> {
-  e.persist()
-  updateFormState((prev)=> ({...prev, [e.target.name]: e.target.value}))
-}
+  const onChange = (e) => {
+    e.persist();
+    updateFormState((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
+  console.log(formState, "formState");
 
   return (
     <div className="App">
       <NavigationBar />
-      <Login />
-   {
-     formType === 'signup' && (
-       <div>
-         <input name='email' onChange={onChange} placeholder="email" />
-         <input name='password' onChange={onChange} placeholder="password" />
-         <button onClick={signUp}>Sign up</button>
-       </div>
-     )
-   }
-      {
-     formType === 'confirmSignUp' && (
-       <div>
-         <input name='authCode' onChange={onChange} placeholder="Confirmation code" />
-         <button onClick={confirmSignUp}>Confirm signup</button>
-       </div>
-     )
-   }
-   {
-     formType === 'signin' && (
-       <div>
-         <input name='email' onChange={onChange} placeholder="email" />
-         <input name='password' onChange={onChange} placeholder="password" />
-         <button onClick={signIn}>Sign In</button>
-       </div>
-     )
-   }
-   {
-     formType === 'welcomeScreen' && (
-       <div>
-         <h1>Hello World, Welcome User!</h1>
-       </div>
-     )
-   }
-
-    </div> 
+      {formType === "signup" && (
+        <Login
+          onClickHandler={signUp}
+          formType={formType}
+          onChange={onChange}
+        />
+      )}
+      {formType === "confirmSignUp" && (
+        <div>
+          {/* <input name='authCode' onChange={onChange} placeholder="Confirmation code" />
+         <button onClick={confirmSignUp}>Confirm signup</button> */}
+          <Login
+            onClickHandler={confirmSignUp}
+            onChange={onChange}
+            formType={formType}
+          />
+        </div>
+      )}
+      {formType === "signin" && (
+        <div>
+          <Login
+            onClickHandler={signIn}
+            formType={formType}
+            onChange={onChange}
+          />
+        </div>
+      )}
+      {formType === "welcomeScreen" && (
+        <div>
+          <h1>Hello World, Welcome User!</h1>
+        </div>
+      )}
+    </div>
   );
 }
 
--- src/container/Login/Login.js
+++ src/container/Login/Login.js
@@ -1,54 +1,78 @@
+import { LockClosedIcon } from "@heroicons/react/solid";
+import Logo from "../../components/Login/logo-01.png";
 
-import { LockClosedIcon } from '@heroicons/react/solid'
-import Logo from '../../components/Login/logo-01.png'
-
-export default function Login() {
+export default function Login({ onClickHandler, formType, onChange }) {
   return (
     <>
-
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <img
-              className="mx-auto h-12 w-auto"
-              src={Logo}
-              alt="Workflow"
-            />
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-
+            <img className="mx-auto h-12 w-auto" src={Logo} alt="Workflow" />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              {`${formType} to your account`}
+            </h2>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <form
+            className="mt-8 space-y-6"
+            action="#"
+            method="POST"
+            onSubmit={(e) => {
+              e.preventDefault();
+              onClickHandler();
+            }}
+          >
             <input type="hidden" name="remember" defaultValue="true" />
-            <div className="rounded-md shadow-sm -space-y-px">
-              <div className='mb-4'>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Email address"
-                />
+            {formType === "confirmSignUp" ? (
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div className="mb-4">
+                  <label htmlFor="email-address" className="sr-only">
+                    Verification Code XXXX
+                  </label>
+                  <input
+                    id="authCode"
+                    name="authCode"
+                    type="text"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Verification Code XXXX"
+                    onChange={onChange}
+                  />
+                </div>
               </div>
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Password"
-                />
+            ) : (
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div className="mb-4">
+                  <label htmlFor="email-address" className="sr-only">
+                    Email address
+                  </label>
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Email address"
+                    onChange={onChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Password"
+                    onChange={onChange}
+                  />
+                </div>
               </div>
-            </div>
+            )}
 
             <div className="flex items-center justify-between">
               {/* <div className="text-sm">
@@ -57,21 +81,23 @@ export default function Login() {
                 </a>
               </div> */}
             </div>
-
             <div>
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                  <LockClosedIcon
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    aria-hidden="true"
+                  />
                 </span>
-                Sign in
+                {formType}
               </button>
             </div>
           </form>
         </div>
       </div>
     </>
-  )
+  );
 }
